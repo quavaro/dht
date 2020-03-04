4d337d9103f5b1c81276569003e7e40017246443
Quavaro <nvaroqua@gmail.com>
2020-03-04 15:56:36 -0500

    limit sensor.js to sensing

--- sensor.js
+++ sensor.js
@@ -2,47 +2,9 @@ const sensor = require("node-dht-sensor").promises;
 const bunyan = require("bunyan");
 const fs = require("fs");
 
-const logPath  = "/var/tmp/dhtsensor.log";
+const logPath  = "var/tmp/dhtsensor.log";
 let lowestTemp = 9999;
 let highestTemp = -9999;
-var myArgs = process.argv.slice(2);
-let filename, userDate;
-
-
-//Usage 
-if(myArgs.length>0){
-    //checks to see if entered file path mathches the one containing log data
-    if(myArgs[0] == logPath){
-        filename = myArgs[0];
-    }
-    else console.log("file path entered does not contain recorded data");
-    userDate = myArgs[1];
-} else {
-    console.log("Enter: [Log file path] [yyyy-mm-dd]")
-}
-
-//read file and parse JSON string to object jsonString
-let logString;
-fs.readFile(filename, 'utf8', (err, jsonString) => {
-    if (err) {
-        console.log("File read failed:", err);
-        return;
-    }
-    try {
-        logString = JSON.parse(jsonString);
-        console.log("JSON string parsed");
-    } catch(err) {
-        console.log('Error parsing JSON string:', err)
-    }
-})
-
-//search through file and compare string
-for(let i = 0; i < logString.length; i++){
-    const date = logString[i].time;
-    if(date.indexOf( userDate ) > -1){
-        console.log(logString[i]);
-    }
-}
 
 
 const log = bunyan.createLogger({
@@ -60,14 +22,15 @@ setInterval(async () => {
         const reading = await sensor.read(22,4);
         const temp = reading.temperature;
         const humid = reading.humidity;
+        const date = new Date();
         if (temp>highestTemp) {
             highestTemp = temp;
         }
         else if(temp<lowestTemp) {
             lowestTemp = temp;
         }
-        log.info(`Highest Temperature ${highestTemp}; Lowest Temperature ${lowestTemp}; humidity ${humid}`);
-        console.log(`Temperature ${temp} humidity ${humid}`);
+        log.info(`{'highTemp': ${highestTemp},'lowTemp': ${lowestTemp},'temperature': '${temperature},' humidity': ${humid},'date': ${date.toLocaleDateString("en-US")}}`);
+        console.log(`Temperature ${temp} | humidity ${humid} | Highest Temperature ${highestTemp} | Lowest Temperature ${lowestTemp} | humidity ${humid} | date ${date.toLocaleDateString("en-US")}`);
     } catch (e) {
         console.log("Error!");
         console.log(e);
